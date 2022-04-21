60f6d2800a1bf02a9c382bf530a3ba825f0b30ed
kidus sintayehu <88981542+kidussintayehu@users.noreply.github.com>
2022-04-21 21:06:46 +0300

    change the localhost request to heroku url

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5050/api/";
+const BASE_URL = "https://tibebmeda.herokuapp.com/api/";
 const TOKEN =  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
